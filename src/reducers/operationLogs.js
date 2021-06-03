8e3ab49e5e990db47f2e9bc98e93d620c663654c
github 26krkz <26krkz@gmail.com>
2021-06-03 23:03:00 +0900

    create operation logs

--- src/reducers/operationLogs.js
+++ src/reducers/operationLogs.js
@@ -3,7 +3,7 @@ import {
   DELETE_ALL_OPERATION_LOGS
 } from '../actions'
 
-const operationLogs = (state= [], action) => {
+const operationLogs = (state = [], action) => {
   switch (action.type) {
     case ADD_OPERATION_LOG:
       const operationLog = {
